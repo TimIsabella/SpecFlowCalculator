42dcc138a250db32508ff1064602a7b3cf82d85d
TimIsabella <timjisabella@gmail.com>
2023-01-23 12:51:27 -0800

    Add project files.

---
+++ SpecFlowCalculator.Specs/StepDefinitions/CalculatorStepDefinitions.cs
@@ -0,0 +1,38 @@
+using FluentAssertions;  //Adds additional extentions like .Should() and .Be()
+
+namespace SpecFlowCalculator.Specs.StepDefinitions
+{
+   [Binding]
+   public sealed class CalculatorStepDefinitions
+   {
+      // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+      private readonly Calculator _calculator = new Calculator();
+      private int _result;
+
+      [Given("the first number is (.*)")]
+      public void GivenTheFirstNumberIs(int number)
+      {
+         _calculator.FirstNumber = number;   //Check that FirstNumber property equals parameter 'number'
+      }
+
+      ///The feature contains an 'and' between these two 'Given's, therefore they both must pass
+
+      [Given("the second number is (.*)")]
+      public void GivenTheSecondNumberIs(int number)
+      {
+         _calculator.SecondNumber = number;  //Check that SecondNumber property equals parameter 'number'
+      }
+
+      [When("the two numbers are added")]
+      public void WhenTheTwoNumbersAreAdded()
+      {
+         _result = _calculator.Add();  //Check that .Add() method of Calculator class is called
+      }
+
+      [Then("the result should be (.*)")]
+      public void ThenTheResultShouldBe(int result)
+      {
+         _result.Should().Be(result);  //Check that the _result set by .Add() method above equals the parameter 'result'
+      }
+   }
+}
